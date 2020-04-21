220599f3ed7648868886b1690cec92f6ef471a3e
mixulidio <mixulidio@gmail.com>
2020-04-21 12:37:52 -0300

    ip prod

--- src/test/java/br/rs/mixulidio/tasks/CheckProdIT.java
+++ src/test/java/br/rs/mixulidio/tasks/CheckProdIT.java
@@ -18,7 +18,7 @@ public class CheckProdIT {
         DesiredCapabilities cap = DesiredCapabilities.chrome();
         WebDriver driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), cap);
         try {
-            driver.navigate().to("http://192.168.99.1:9999/tasks/");
+            driver.navigate().to("http://192.168.99.100:9999/tasks/");
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             String versao = driver.findElement(By.id("version")).getText();
             Assert.assertTrue(versao.startsWith("build"));
